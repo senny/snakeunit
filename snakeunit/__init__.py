5dd5c3d2e8b9e8f073a3405180c72d3be205b5d2
Yves Senn <yves.senn@gmail.com>
2013-02-26 16:18:36 +0100

    refactoring to TestResult objects

--- snakeunit/__init__.py
+++ snakeunit/__init__.py
@@ -12,6 +12,34 @@ class FailedAssertion(Exception):
     def __str__(self):
         return repr(self.value)
 
+class TestResult:
+
+    def __init__(self, name, state, exception = None):
+        self.name = name
+        self.state = state
+        self.exception = exception
+
+    def didPass(self):
+        return self.state == 'passed'
+
+    def wasSkipped(self):
+        return self.state == 'skipped'
+
+    def didFail(self):
+        return self.state == 'failed'
+
+    @classmethod
+    def skipped(klass, name):
+        return klass(name, 'skipped')
+
+    @classmethod
+    def passed(klass, name):
+        return klass(name, 'passed')
+
+    @classmethod
+    def failed(klass, name, exception):
+        return klass(name, 'failed', exception)
+
 class TestCase:
     TEST_NAME_REGEXP = re.compile('^test')
 
@@ -24,7 +52,6 @@ class TestCase:
 
         return tests;
 
-
     # ASSERTIONS
     def assertEqual(self, expected, actual, message = None):
         if expected != actual:
@@ -32,15 +59,18 @@ class TestCase:
             raise FailedAssertion(message)
 
     @classmethod
-    def run(klass, output):
+    def run(klass):
+        testResults = []
         instance = klass();
         for name, test in instance._tests().items():
+            result = None
             try:
                 test()
-                output.write('.')
-            except FailedAssertion:
-                output.write('F')
-                raise
+                testResults.append(TestResult.passed(name))
+            except FailedAssertion, e:
+                testResults.append(TestResult.failed(name, e))
+
+        return testResults
 
 class Runner:
     def __init__(self, output = sys.stdout):
@@ -51,6 +81,21 @@ class Runner:
         self.testCases.append(testCase)
 
     def run(self):
+        suiteResults = []
         for testCase in self.testCases:
-            testCase.run(self.output)
+            results = testCase.run()
+            for result in results:
+                if result.didPass():
+                    self.output.write('.')
+                elif result.didFail():
+                    self.output.write('F')
+
+            suiteResults.extend(results)
+
+        self.output.write("\n")
+        for result in suiteResults:
+            if result.didFail():
+                self.output.write(result.name + "\n")
+                self.output.write(str(result.exception) + "\n")
+                self.output.write("\n")
         self.output.write("\n")
--- test/test_main.py
+++ test/test_main.py
@@ -10,18 +10,45 @@ class MainTestCase(snakeunit.TestCase):
     def testSecond(self):
         None
 
+class TestResultTestCase(snakeunit.TestCase):
+
+    def testHasAName(self):
+        result = snakeunit.TestResult.passed('testMyExample')
+        self.assertEqual('testMyExample', result.name)
+
+    def testSuccessfulResult(self):
+        result = snakeunit.TestResult.passed('testExample')
+        self.assertEqual(True, result.didPass())
+        self.assertEqual(False, result.didFail())
+        self.assertEqual(False, result.wasSkipped())
+
+    def testFailedResult(self):
+        result = snakeunit.TestResult.failed('testExample', AssertionError('did not work'))
+        self.assertEqual(True, result.didFail())
+        self.assertEqual(False, result.didPass())
+        self.assertEqual(False, result.wasSkipped())
+
+    def testSkippedResult(self):
+        result = snakeunit.TestResult.skipped('testExample')
+        self.assertEqual(False, result.didFail())
+        self.assertEqual(False, result.didPass())
+        self.assertEqual(True, result.wasSkipped())
+
 class RunnerTestCase(snakeunit.TestCase):
 
     class ExampleTest(snakeunit.TestCase):
-        def testTruth(self):
+        def testSuccess(self):
             self.assertEqual(True, True)
+
+        def testFailure(self):
+            self.assertEqual(False, True)
 
     def testSingleTestCase(self):
         output = StringIO.StringIO()
         runner = snakeunit.Runner(output)
         runner.register(RunnerTestCase.ExampleTest)
         runner.run()
-        self.assertEqual(".\n", output.getvalue())
+        self.assertEqual("F.", output.getvalue().split("\n")[0])
 
 class AssertionsTestCase(snakeunit.TestCase):
 
@@ -46,6 +73,7 @@ class AssertionsTestCase(snakeunit.TestCase):
 
 runner = snakeunit.Runner()
 runner.register(MainTestCase)
+runner.register(TestResultTestCase)
 runner.register(AssertionsTestCase)
 runner.register(RunnerTestCase)
 runner.run()
